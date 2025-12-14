4dbb8a33f5e461b7e20647d7b72363b78bf60463
UltraM704 <mansib704@gmail.com>
2025-12-14 17:46:46 -0500

    data_quality_test.py

--- data_quality_test.py
+++ data_quality_test.py
@@ -3,8 +3,8 @@ import pandas as pd
 # --------------------------------------------------------
 # Load data
 # --------------------------------------------------------
-DEVICE_FILE = r"C:\Users\megau\Downloads\9-13.xlsx"
-OUTPUT_FILE = r"C:\Users\megau\Downloads\9-13-new.xlsx"
+DEVICE_FILE = r"C:\Users\megau\Downloads\9-13.xlsx"     # make sure to change the file location
+OUTPUT_FILE = r"C:\Users\megau\Downloads\9-13-new.xlsx" # this one too
 
 df_raw = pd.read_excel(DEVICE_FILE)
 print(f"📥 Total devices loaded: {len(df_raw)}")
@@ -32,4 +32,5 @@ df_clean = df_raw[~broken_mask].copy()
 df_clean.to_excel(OUTPUT_FILE, index=False)
 
 print(f"✅ Clean devices saved: {len(df_clean)}")
+
 print(f"📁 New file created: {OUTPUT_FILE}")
